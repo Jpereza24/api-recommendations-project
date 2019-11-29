738a400c74691bd6670f6f0d6d88045bc1d2cbfb
Joaquin <jperezdeayala@gmail.com>
2019-11-29 19:11:22 +0100

    borrar mierdapi

--- api.py
+++ api.py
@@ -2,15 +2,3 @@ from bottle import route, run, get, post, request,error
 import random
 import os
 
-@get("/")
-def nombre():
-    return {"nombre": random.choice(["Joaquin","Mustafa","tu puta madre"])}
-
-@error(404)
-def error404(error):
-    return {"error": "mierda"}
-
-port = int(os.getenv("PORT", 8080))
-print(f"Running server {port}...")
-
-run(host="0.0.0.0", port=port, debug=True)
