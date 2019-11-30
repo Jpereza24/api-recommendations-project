deb5c0df5852744d41e17d4b0f105f7f40ee9237
Joaquin <jperezdeayala@gmail.com>
2019-11-30 20:41:27 +0100

    fin sabado

--- api.py
+++ api.py
@@ -19,7 +19,16 @@ def createuser():
     }
     coll.insert_one(new_user)
 
-
+@post('chat/<chat_id>/addmessage')
+def createMessage(chat_id):
+    message = str(request.forms.get("message"))
+    new_id = coll.distinct("idMessage")[-1] + 1
+    new_message = {
+        "idChat": chat_id,
+        "idMessage":new_id,
+        "text" : message
+    }
+    coll.insert_one(new_message)
 
 db, coll = m.connectCollection('apiproject', 'chats')
 run(host="0.0.0.0", port=8080, debug=True)
