010226d0fbc27f61ae05fefccda1605480250c55
Joaquin <jperezdeayala@gmail.com>
2019-11-30 17:45:03 +0100

    salen cosas

--- api.py
+++ api.py
@@ -7,7 +7,7 @@ from pymongo import MongoClient
 from dotenv import load_dotenv
 load_dotenv()
 
-password = os.getenv('mongoURL')
+password = os.getenv("MONGOURL")
 client = MongoClient(password)
 db = client['apiproject']
 coll = db['chats']
@@ -15,4 +15,9 @@ coll = db['chats']
 @get("/")
 def index():
     return dumps(coll.find())
-run(host="0.0.0.0", port=8080)
+run(host="127.0.0.1", port=8080)
+
+@post('/createuser/<username>')
+def userCreation(username):
+    ''' Insertas un username y te devuelve el user_id'''
+    return user_id
